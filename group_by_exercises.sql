e14745cf04c998471043e5a9c2a8420c865fbf0f
“Cory <coryurton@gmail.com>
2022-07-01 16:41:57 -0500

    aliases exercise complete

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -20,3 +20,54 @@ FROM employees
 WHERE first_name In ('Irena', 'Vidya', 'Maya')
 GROUP BY gender
 ORDER BY gender;
+
+
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+LIMIT 25;
+
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+ORDER BY full_name
+LIMIT 25;
+
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+GROUP BY full_name, last_name
+ORDER BY last_name
+LIMIT 115;
+
+
+
+SELECT hire_date, COUNT(*)
+FROM employees
+GROUP BY hire_date
+ORDER BY COUNT(*) DESC
+LIMIT 10;
+# now with alias:
+SELECT hire_date, COUNT(*) as number_hired
+FROM employees
+GROUP BY hire_date
+ORDER BY number_hired DESC
+LIMIT 10;
+
+
+
+SELECT count(*), CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees AS emp
+GROUP BY full_name, last_name
+ORDER BY last_name
+LIMIT 25;
+
+
+
+SELECT birth_date, CONCAT(first_name, ' ', last_name) AS full_name, gender
+FROM employees
+WHERE hire_date='1985-01-01'
+  AND full_name='Arie Staelin';
+# cant do alias in where clause
+
+
