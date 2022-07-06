cc6d2fa40b49e4ca2c586f32431c8d78c8a99a80
“Cory <coryurton@gmail.com>
2022-07-06 10:25:10 -0500

    bonus #1 completed sub-queries

--- subQs_lec.sql
+++ subQs_lec.sql
@@ -1,35 +1,50 @@
 use employees;
 
 # find info for depts where dept named Customer Service
-select dept_no FROM departments WHERE  dept_name = 'Customer Service';
+select dept_no
+FROM departments
+WHERE dept_name = 'Customer Service';
 
-SELECT emp_no FROM dept_manager WHERE dept_no IN (
-    SELECT dept_no FROM departments WHERE dept_name = 'Sales'
-    );
+SELECT emp_no
+FROM dept_manager
+WHERE dept_no IN (SELECT dept_no
+                  FROM departments
+                  WHERE dept_name = 'Sales');
 # or ....
-SELECT emp_no FROM dept_manager WHERE dept_no IN (
+SELECT emp_no
+FROM dept_manager
+WHERE dept_no IN (
     'd009'
-);
+    );
 # but inaccurate
 
 SELECT (SELECT emp_no FROM dept_manager LIMIT 1) as emp_no;
 
 
 # get dept managers emp number of the sales department
-SELECT emp_no from dept_manager dm WHERE dept_no IN (
-    select dept_no FROM departments WHERE dept_name = 'Sales'
-                                      AND to_date > now()
-    );
+SELECT emp_no
+from dept_manager dm
+WHERE dept_no IN (select dept_no
+                  FROM departments
+                  WHERE dept_name = 'Sales'
+                    AND to_date > now());
 
 
 # Get FIRST TWO employee's salaries, concatenated
-SELECT emp_no FROM employees LIMIT 2;
+SELECT emp_no
+FROM employees
+LIMIT 2;
 
 SELECT CONCAT(
-    (SELECT MAX(salary) FROM salaries
-                           WHERE emp_no = 10001
-), ' ', (SELECT MAX(salary) FROM salaries WHERE emp_no = 10002), ' '
+               (SELECT MAX(salary)
+                FROM salaries
+                WHERE emp_no = 10001), ' ',
+               (SELECT MAX(salary) FROM salaries WHERE emp_no = 10002), ' '
            ) AS TopPeople;
 
 
 # get names of emps with th
+
+# female male salary comparison
+# year over year diff between them
+
--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,28 +1,38 @@
 use employees;
 
 # 1
-SELECT * from employees
-where hire_date like (
-    SELECT hire_date
-    from employees
-    where emp_no = 101010
-    );
+SELECT *
+from employees
+where hire_date like (SELECT hire_date
+                      from employees
+                      where emp_no = 101010);
 
 # 2   with/without group by for total / unique titles
 SELECT title
 from titles
-WHERE emp_no IN (
-    SELECT employees.emp_no
-    FROM employees
-    WHERE employees.first_name = 'Aamod'
-    )
+WHERE emp_no IN (SELECT employees.emp_no
+                 FROM employees
+                 WHERE employees.first_name = 'Aamod')
 GROUP BY title;
 
 SELECT employees.first_name, employees.last_name
 FROM employees
-WHERE employees.emp_no IN (
-    SELECT dept_manager.emp_no
-    FROM dept_manager
-    WHERE to_date LIKE '999%'
-    )
-AND employees.gender = 'F'
+WHERE employees.emp_no IN (SELECT dept_manager.emp_no
+                           FROM dept_manager
+                           WHERE to_date LIKE '999%')
+  AND employees.gender = 'F';
+
+
+# bonus 1  find dept names currently having female managers
+
+SELECT dept_name
+from departments
+WHERE dept_no IN (SELECT dept_manager.dept_no
+                  FROM dept_manager
+                  WHERE dept_manager.emp_no IN (select dept_manager.emp_no
+                                                from dept_manager
+                                                WHERE dept_manager.to_date > now()
+                                                  AND dept_manager.emp_no IN (select employees.emp_no
+                                                                              FROM employees
+                                                                              where employees.gender = 'F')))
+
