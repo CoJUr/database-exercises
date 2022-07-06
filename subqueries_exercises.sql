add832626ce588ab56d3adc68152a52203947e30
“Cory <coryurton@gmail.com>
2022-07-05 22:26:59 -0500

    sub-queries complete

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -18,3 +18,11 @@ WHERE emp_no IN (
     )
 GROUP BY title;
 
+SELECT employees.first_name, employees.last_name
+FROM employees
+WHERE employees.emp_no IN (
+    SELECT dept_manager.emp_no
+    FROM dept_manager
+    WHERE to_date LIKE '999%'
+    )
+AND employees.gender = 'F'
